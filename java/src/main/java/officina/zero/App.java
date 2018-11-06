8fad111ff6ccdbae146741cfe9d15b0255610433
Christian Geymonat <chris.geymo@gmail.com>
2018-11-06 17:03:58 +0100

    java version

---
+++ java/src/main/java/officina/zero/App.java
@@ -0,0 +1,13 @@
+package officina.zero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
